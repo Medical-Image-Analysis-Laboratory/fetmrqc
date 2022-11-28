1c2722ceb65ca588fba0b3dfb260370b3dc0b1d6
Thomas Sanchez <sanchez.thomas056@gmail.com>
2022-11-28 16:36:56 +0100

    Split the mask_pattern in two components: the base path and the pattern itself, to allow for more flexibly searching directories without needing to specify the entire pattern multiple times.

--- fetal_brain_qc/cli/run_list_and_anon_bids.py
+++ fetal_brain_qc/cli/run_list_and_anon_bids.py
@@ -2,7 +2,8 @@ def main():
     import argparse
     from fetal_brain_qc.list_bids import list_bids
     from fetal_brain_qc.anon_bids import anonymize_bids_csv
-    from fetal_brain_qc.definitions import MASK_PATTERN_LIST
+    from fetal_brain_qc.definitions import MASK_PATTERN, MANU_BASE, AUTO_BASE
+    import os
 
     p = argparse.ArgumentParser(
         description=(
@@ -22,15 +23,25 @@ def main():
     p.add_argument(
         "--mask-patterns",
         help=(
-            "List of patterns to find the LR masks corresponding to the LR series.\n "
+            "Pattern(s) to find the LR masks corresponding to the LR series.\n "
             'Patterns will be of the form "sub-{subject}[/ses-{session}][/{datatype}]/sub-{subject}'
             '[_ses-{session}][_acq-{acquisition}][_run-{run}]_{suffix}.nii.gz", and the different fields will be '
-            "substituted based on the structure of bids_dir. The code will attempt to find the mask corresponding "
-            "to the first pattern, and if it fails, will attempt the next one, etc. If no masks are found, a warning "
-            "message will be displayed for the given subject, session and run. "
+            "substituted based on the structure of bids_dir. The base directory from which the search will be run "
+            "can be changed with `--mask-pattern-base`."
         ),
         nargs="+",
-        default=MASK_PATTERN_LIST,
+        default=[MASK_PATTERN],
+    )
+
+    p.add_argument(
+        "--mask-pattern-base",
+        help=(
+            "Base folder(s) from which the LR masks must be listed.\n "
+            "The method will look for masks at `mask-pattern-base`/`mask-patterns`. "
+            "In this case, both `mask-patterns` and `mask-pattern-base` should be of the same length."
+        ),
+        nargs="+",
+        default=None,
     )
 
     p.add_argument(
@@ -51,9 +62,25 @@ def main():
 
     args = p.parse_args()
     print("Running list_bids.")
+    # Constructing patterns.
+    if args.mask_pattern_base:
+        print(args.mask_pattern_base, args.mask_patterns)
+        assert len(args.mask_pattern_base) == len(
+            args.mask_patterns
+        ), "mask_pattern_base and mask_patterns have different lengths."
+        mask_patterns = [
+            os.path.join(os.path.abspath(b), m)
+            for b, m in zip(args.mask_pattern_base, args.mask_patterns)
+        ]
+    else:
+        mask_pattern_base = [MANU_BASE, AUTO_BASE]
+        mask_patterns = [
+            base + args.mask_patterns[0] for base in mask_pattern_base
+        ]
+    print(mask_patterns)
     list_bids(
         args.bids_dir,
-        args.mask_patterns,
+        mask_patterns,
         bids_csv=args.out_csv,
     )
     if args.anonymize_name:
